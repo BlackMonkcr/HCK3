1591c02de5df20dc9e3a47c18eb836e9e1b9c324
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-29 00:13:30 -0500

    Create Project

---
+++ HCK3/src/test/java/com/example/hck3/Hck3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hck3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hck3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
