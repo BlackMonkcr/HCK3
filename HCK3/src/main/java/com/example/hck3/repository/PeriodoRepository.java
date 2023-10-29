64e5cded6009ea480c6a8587708dd7c83b0bc483
Patricia Nuñez <patricia.nunez@utec.edu.pe>
2023-10-29 17:23:18 -0500

    jashjhaj

---
+++ HCK3/src/main/java/com/example/hck3/repository/PeriodoRepository.java
@@ -0,0 +1,6 @@
+package com.example.hck3.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.hck3.Periodo;
+
+public interface PeriodoRepository extends JpaRepository<Periodo, Long> {}
+    
